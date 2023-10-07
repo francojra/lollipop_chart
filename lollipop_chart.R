e36583aa7e2352d11b45d25bd9f3982a334eb8b1
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-10-06 23:53:37 -0300

    Add files via upload

--- lollipop_chart.R
+++ lollipop_chart.R
@@ -12,3 +12,14 @@
 
 ### Anotações é ponto chave na visualização de dados: elas permitem ao leitor focar
 ### sobre a mensagem principal que quer ser transmitida.
+
+### Se um ou poucos grupos interessam a você,é uma boa ideia destacar eles em um gráfico.
+### O leitor irá entender mais rapidamente qual a história por trás do gráfico.
+
+### Sendo assim, você pode usar o ifelse para mudar tamanho, cor e transparência, entre
+### outros aesthetics. Além disso, é ainda mais esclarecedor adicionar textos ao gráfico.
+
+# Criando o gráfico ------------------------------------------------------------------------------------------------------------------------
+
+
+
